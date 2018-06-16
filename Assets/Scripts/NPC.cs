392e88192a925e38aecfd21647bb469accbbc99e
Sarah <tan.sh.sarah@gmail.com>
2018-06-16 15:31:20 -0700

    Added basic NPC movement logic and NPC manager to spawn them

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float minSpeed;
+    [SerializeField]
+    private float maxSpeed;
+    
+    [SerializeField]
+    private float rotationalSpeed;
+
+    [SerializeField]
+    private float minTimeBeforeDirectionChange;
+    [SerializeField]
+    private float maxTimeBeforeDirectionChange;
+
+    private float currentSpeed;
+    private float nextDirectionChangeTime;
+    
+    private Quaternion targetRotation = Quaternion.identity;
+
+    #region Properties
+
+    private Vector2 Position2D
+    {
+        get { return new Vector2(transform.position.x, transform.position.y); }
+    }
+
+    private Vector2 Right2D
+    {
+        get { return new Vector2(transform.right.x, transform.right.y); }
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        transform.rotation = GetRandom2DRotation();
+        nextDirectionChangeTime = GetNextDirectionChangeTime();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime* rotationalSpeed);
+        rb.MovePosition(Position2D + Right2D * maxSpeed * Time.deltaTime); 
+
+        if (Time.time > nextDirectionChangeTime)
+        {
+            targetRotation = GetRandom2DRotation();
+            nextDirectionChangeTime = GetNextDirectionChangeTime();
+        }
+    }
+
+    #region Helpers
+
+    private float GetNextDirectionChangeTime()
+    {
+        return Time.time + Random.Range(minTimeBeforeDirectionChange, maxTimeBeforeDirectionChange);
+    }
+
+    public Quaternion GetRandom2DRotation()
+    {
+        return Quaternion.Euler(new Vector3(0f, 0f, Random.Range(0f, 360f)));
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/NPCManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : Singleton<NPCManager> {
+
+    [SerializeField]
+    private int maxNumberOfNPCs;
+    [SerializeField]
+    private NPC NPCPrefab;
+
+    private List<NPC> allNPCs = new List<NPC>();
+
+    private void Start()
+    {
+        while(allNPCs.Count < maxNumberOfNPCs)
+        {
+            var npc = Instantiate(NPCPrefab);
+            npc.transform.parent = transform;
+            allNPCs.Add(npc);
+        }
+    }
+}
